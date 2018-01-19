6af094b4980417109248a9e758ecd6fbfc3160cc
evandermeijde <evandermeijde@hotmail.com>
2018-01-19 09:42:24 +0100

    Eerste versie

---
+++ GIS_in_R_voorbeeld_Den_Haag.R
@@ -0,0 +1,4 @@
+download.file("http://geoportaal-ddh.opendata.arcgis.com/datasets/9cca53a6a4094a0b80e964b181a484ad_3.zip","blokverwarming.zip", mode="wb")
+unzip("blokverwarming.zip")
+download.file("http://geoportaal-ddh.opendata.arcgis.com/datasets/ed833f69fb82463ea63d37be00c356c3_0.zip", "energielabels.zip", mode="wb")
+unzip("energielabels.zip")
